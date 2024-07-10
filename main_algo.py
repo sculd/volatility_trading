0ca3ab837a962bb3f01264815576272a49446c81
Hyungjun Lim <sculd3@gmail.com>
2024-07-10 19:02:54 -0400

    add main*py

---
+++ main_algo.py
@@ -0,0 +1,78 @@
+from pandas_market_calendars import get_calendar
+
+import pandas as pd
+
+import data.polygon
+import data.intraday
+import data.daily
+import algo.option_spread
+
+import algo.expectation
+import algo.volatility
+
+ticker_spy = "SPY"
+ticker_spx = "I:SPX"
+ticker_goog = "GOOG"
+ticker_sbux = "SBUX"
+ticker_vix1d = "I:VIX1D"
+options_ticker = "SPX"
+
+
+calendar = get_calendar("NYSE")
+trading_dates = calendar.schedule(
+    start_date = "2023-05-01", 
+    end_date = "2024-07-02"
+    #end_date = (datetime.today()-timedelta(days = 1))
+).index.strftime("%Y-%m-%d").values
+
+
+df_spy_daily_history = pd.read_pickle('market_data/df_spy_daily_history.pkl')
+df_goog_daily_history = pd.read_pickle('market_data/df_goog_daily_history.pkl')
+df_sbux_daily_history = pd.read_pickle('market_data/df_sbux_daily_history.pkl')
+
+df_spy_intraday_history = pd.read_pickle('market_data/df_spy_intraday_history.pkl')
+df_vix1d_intraday_history = pd.read_pickle('market_data/df_vix1d_intraday_history.pkl')
+df_spx_intraday_history = pd.read_pickle('market_data/df_spx_intraday_history.pkl')
+df_goog_intraday_history = pd.read_pickle('market_data/df_goog_intraday_history.pkl')
+df_sbux_intraday_history = pd.read_pickle('market_data/df_sbux_intraday_history.pkl')
+
+df_spx_call_options_history = pd.read_pickle('market_data/df_spx_call_options_history.pkl')
+df_spx_put_options_history = pd.read_pickle('market_data/df_spx_put_options_history.pkl')
+
+
+
+df_spy_market_open_history = data.daily.get_df_market_open_or_close_history_from_intraday_history(df_spy_intraday_history, "open")
+df_vix1d_market_open_history = data.daily.get_df_market_open_or_close_history_from_intraday_history(df_vix1d_intraday_history, "open")
+df_spx_market_open_history = data.daily.get_df_market_open_or_close_history_from_intraday_history(df_spx_intraday_history, "open")
+
+
+
+
+# intraday optinos
+'''
+df_spx_daily_expectation = algo.expectation.get_df_daily_expectation(df_spx_market_open_history)
+df_spx_otm_call_options_spread_history = algo.option_spread.get_df_otm_options_spread_history(df_spx_daily_expectation, df_spx_call_options_history, "call")
+df_spx_otm_put_options_spread_history = algo.option_spread.get_df_otm_options_spread_history(df_spx_daily_expectation, df_spx_put_options_history, "put")
+
+df_spx_otm_call_options_spread_history.to_pickle('market_data/df_spx_otm_call_options_spread_history.pkl')
+df_spx_otm_put_options_spread_history.to_pickle('market_data/df_spx_otm_put_options_spread_history.pkl')
+'''
+df_spx_otm_call_options_spread_history = pd.read_pickle('market_data/df_spx_otm_call_options_spread_history.pkl')
+df_spx_otm_put_options_spread_history = pd.read_pickle('market_data/df_spx_otm_put_options_spread_history.pkl')
+
+
+# volatility
+'''
+df_goog_atm_vol_history = algo.volatility.get_df_atm_vol_history("GOOG")
+df_goog_atm_vol_history.to_pickle('market_data/df_goog_atm_vol_history.pkl')
+
+df_sbux_atm_vol_history = algo.volatility.get_df_atm_vol_history("SBUX")
+df_sbux_atm_vol_history.to_pickle('market_data/df_sbux_atm_vol_history.pkl')
+'''
+df_goog_atm_vol_history = pd.read_pickle('market_data/df_goog_atm_vol_history.pkl')
+df_sbux_atm_vol_history = pd.read_pickle('market_data/df_sbux_atm_vol_history.pkl')
+
+
+
+
+
---
+++ main_cache.py
@@ -0,0 +1,68 @@
+from pandas_market_calendars import get_calendar
+
+import data.polygon
+import data.intraday
+import data.daily
+import data.option_contracts
+
+import algo.option_spread
+import algo.volatility
+
+ticker_spy = "SPY"
+ticker_spx = "I:SPX"
+ticker_goog = "GOOG"
+ticker_sbux = "SBUX"
+ticker_vix1d = "I:VIX1D"
+options_ticker = "SPX"
+
+
+calendar = get_calendar("NYSE")
+trading_dates = calendar.schedule(
+    start_date = "2023-05-01", 
+    end_date = "2024-07-02"
+    #end_date = (datetime.today()-timedelta(days = 1))
+).index.strftime("%Y-%m-%d").values
+
+
+# daily
+
+def get_cache_df_daily_history(ticker, df_name, date_from_str = "2020-01-01", date_to_str = "2024-07-02"):
+    df_daily_history = data.polygon.polygon_result_to_dataframe(data.polygon.get_polygon_result_dict(
+        data.polygon.get_polygon_range_query_url(ticker, date_from_str, date_to_str)))
+    df_daily_history.index = df_daily_history.index.strftime("%Y-%m-%d")
+    df_daily_history.to_pickle(f'market_data/{df_name}.pkl')
+    return df_daily_history
+
+df_spy_daily_history = get_cache_df_daily_history(ticker_spy, "df_spy_daily_history")
+df_spx_daily_history = get_cache_df_daily_history(ticker_spx, "df_spx_daily_history")
+df_goog_daily_history = get_cache_df_daily_history(ticker_goog, "df_goog_daily_history")
+df_sbux_daily_history = get_cache_df_daily_history(ticker_sbux, "df_sbux_daily_history")
+
+
+# intraday
+
+def get_cache_df_intraday_history(ticker, df_name):
+    df_intraday_history = data.intraday.get_df_intrady_history(ticker)
+    if "index" in df_intraday_history.columns:
+        df_intraday_history = df_intraday_history.drop(columns=["index"])
+    df_intraday_history.to_pickle(f'market_data/{df_name}.pkl')
+    return df_intraday_history
+
+df_spy_intraday_history = get_cache_df_intraday_history(ticker_spy, "df_spy_intraday_history")
+df_vix1d_intraday_history = get_cache_df_intraday_history(ticker_vix1d, "df_vix1d_intraday_history")
+df_spx_intraday_history = get_cache_df_intraday_history(ticker_spx, "df_spx_intraday_history")
+df_goog_intraday_history = get_cache_df_intraday_history(ticker_goog, "df_goog_intraday_history")
+df_sbux_intraday_history = get_cache_df_intraday_history(ticker_sbux, "df_sbux_intraday_history")
+
+
+# options contracts
+
+df_spx_call_options_history = data.option_contracts.get_df_options_history("SPX", "call")
+df_spx_call_options_history.to_pickle('market_data/df_spx_call_options_history.pkl')
+
+df_spx_put_options_history = data.option_contracts.get_df_options_history("SPX", "put")
+df_spx_put_options_history.to_pickle('market_data/df_spx_put_options_history.pkl')
+
+
+
+
